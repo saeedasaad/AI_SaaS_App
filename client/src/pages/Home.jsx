5bf69d64136cc7e1017c66aedcbb92cab431e77c
Saeeda <saeeda123@gmail.com>
2025-09-16 15:14:12 +0500

    Initial commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
