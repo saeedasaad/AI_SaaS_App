343bb9cd4517d176e92bf3ef2d5f58a64748fdcc
Saeeda <saeeda123@gmail.com>
2025-09-22 21:38:48 +0500

    Initial commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import AiTools from '../components/AiTools';
+import Testimonial from '../components/Testimonial';
 
 export default function Home() {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <AiTools/>
+      <Testimonial />
     </>
   );
 }
